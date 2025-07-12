dfe362e8c563927a1b8cd873826b0833e737e647
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-07-12 21:37:56 +0700

    refactor: to async and await

--- script.js
+++ script.js
@@ -11,27 +11,12 @@ btnNewImage.addEventListener("click", () => {
   console.log(valueInput);
 });
 
-function getData(gifsName = "cats") {
-  fetch(
+async function getData(gifsName = "cats") {
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=xDfaUM063KEUbwjvfGC2bYo7XF09utMC&s=${gifsName}`,
     { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-      let responseData = response.data;
-      if (responseData.length !== 0) {
-        img.src = response.data.images.original.url;
-        return;
-      } else {
-        throw new Error("Keyword Not Found!");
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-      errorMessageField.textContent = error.message;
-    });
+  );
+  const promiseResponse = await response.json();
+  img.src = promiseResponse.data.images.original.url;
 }
 getData();
